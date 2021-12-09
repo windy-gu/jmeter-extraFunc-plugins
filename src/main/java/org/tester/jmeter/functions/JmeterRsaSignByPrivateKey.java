22bde932ee192ae93b7db75d3e0d0971ebacb8bb
windy-gu <408563133@qq.com>
2021-12-09 16:27:16 +0800

    添加 rsa 私钥加密

---
+++ src/main/java/org/tester/jmeter/functions/JmeterRsaSignByPrivateKey.java
@@ -0,0 +1,102 @@
+package org.tester.jmeter.functions;
+
+import lombok.SneakyThrows;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jmeter.common.json.JsonUtil;
+import org.apache.jmeter.engine.util.CompoundVariable;
+import org.apache.jmeter.functions.AbstractFunction;
+import org.apache.jmeter.functions.InvalidVariableException;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.samplers.Sampler;
+import org.slf4j.Logger;
+import org.apache.jmeter.config.Argument;
+import org.apache.jmeter.config.Arguments;
+import org.tester.jmeter.functions.core.RsaSignByPrivateKey;
+import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
+
+
+import java.util.*;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * UrlEncode
+ * @author Windy
+ * @date 2021-5-02 13:15
+ */
+@SuppressWarnings("unchecked")
+public class JmeterRsaSignByPrivateKey extends AbstractFunction {
+
+    /**
+     * function描述，在函数助手中函数参数中显示内容
+     */
+    private static final Logger log = LoggerFactory.getLogger(JmeterRsaSignByPrivateKey.class);
+    private static final List<String> DESC = new LinkedList<String>();
+
+    static {
+        DESC.add("请输入待加签的数据：格式(String)");
+        DESC.add("请输入privateKey：");
+    }
+
+
+    private CompoundVariable unSignData;
+    private CompoundVariable privateKey;
+
+    /**
+     * function执行的主题
+     * @param sampleResult
+     * @param sampler
+     * @return 返回sign加密后的数据
+     * @throws InvalidVariableException
+     */
+    @SneakyThrows
+    @Override
+    public String execute(SampleResult sampleResult, Sampler sampler) throws InvalidVariableException {
+        if (!(sampler instanceof HTTPSamplerProxy)) {
+            log.error("RsaSignByPrivateKey函数目前仅支持在HTTPSampler下使用");
+            return "error";
+        }
+
+        String dataSign = "";
+        String unSignData = this.unSignData.execute().trim();
+        String privateKey = this.privateKey.execute().trim();
+        log.info("开发平台待加签内容={}", unSignData);
+        dataSign = RsaSignByPrivateKey.rsaSignByPrivateKey(unSignData, privateKey);
+        return dataSign;
+    }
+
+    /**
+     *
+     * @param parameters 入参
+     * @throws InvalidVariableException
+     */
+    @Override
+    public void setParameters(Collection<CompoundVariable> parameters) throws InvalidVariableException {
+        // 定义调用时，所需要的入参
+        checkParameterCount(parameters, 2, 2);
+        unSignData = (CompoundVariable) parameters.toArray()[0];
+        privateKey = (CompoundVariable) parameters.toArray()[1];
+    }
+
+    /**
+     * 提供jmeter函数助手显示的下来选项名称
+     **/
+    @Override
+    public String getReferenceKey() {
+        return "__RsaSignByPrivateKey";
+    }
+
+    /**
+     * function的描述
+     */
+    public List<String> getArgumentDesc() {
+        return DESC;
+    }
+
+}
+
---
+++ src/main/java/org/tester/jmeter/functions/core/RsaSignByPrivateKey.java
@@ -0,0 +1,153 @@
+package org.tester.jmeter.functions.core;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.binary.Base64;
+import java.security.PrivateKey;
+import java.security.KeyFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.Signature;
+import java.util.*;
+import java.security.interfaces.RSAPrivateKey;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jmeter.common.json.JsonUtil;
+//import org.slf4j.Logger;
+import org.apache.logging.log4j.util.PropertySource;
+import sun.misc.BASE64Decoder;
+
+public class RsaSignByPrivateKey {
+    public static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
+    /**
+     * RSA 私钥加签
+     * @author Windy
+     */
+
+    public static String rsaSignByPrivateKey(String data, String privateKey) throws Exception {
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        byte[] bytes = Base64.decodeBase64(privateKey);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(bytes);
+        PrivateKey privateKey1 = keyFactory.generatePrivate(keySpec);
+        signature.initSign(privateKey1);
+
+        // 序列化，将json格式
+        Map<Object, Object> jsonUnSignDate = null;
+        if (StringUtils.isNotBlank(data)){
+            jsonUnSignDate = JsonUtil.fromJson(data, JsonUtil.mapType);
+        }
+        Map<Object, Object> sortedUnSignDate = sortMapByKey(jsonUnSignDate);
+        StringBuffer orderedSB = new StringBuffer();
+        if (sortedUnSignDate != null){
+            sortedUnSignDate.forEach((key, value)  -> traverse(orderedSB, key, value));
+        }
+        String orderedRequests = orderedSB.substring(0, orderedSB.length() - 1);
+
+        System.out.println("UNEnsign data test:" + orderedRequests);
+
+        if (StringUtils.isBlank(orderedRequests)) {
+            return "";
+        }
+
+
+        byte[] bytes_1 = orderedRequests.getBytes(CharEncoding.UTF_8);
+        signature.update(bytes_1);
+        byte[] signed = signature.sign();
+        return Base64.encodeBase64String(signed);
+    }
+
+    /**
+     * 递归遍历报文并排序，排序完成后拼接字段
+     */
+    @SuppressWarnings("unchecked")
+    private static void traverse(StringBuffer sb, Object key, Object value) {
+        if (value instanceof Map) {
+            sb.append(key).append("=").append(traverseMap((Map<Object, Object>) value)).append("&");
+        } else if (value instanceof List) {
+            sb.append(key).append("=").append(traverseList((List<Object>) value)).append("&");
+        } else {
+            sb.append(key).append("=").append(value).append("&");
+        }
+    }
+
+    /**
+     * 递归遍历报文并排序，仅针对json中{}的操作，排序完成后拼接字段
+     */
+    @SuppressWarnings("unchecked")
+    private static void traverseInsideMap(StringBuffer sb, Object key, Object value) {
+
+        if (value instanceof Map) {
+            sb.append(key).append("=").append(traverseMap((Map<Object, Object>) value)).append("&");
+        } else if (value instanceof List) {
+            sb.append(key).append("=").append(traverseList((List<Object>) value)).append("&");
+        } else {
+            sb.append("\"").append(key).append("\":\"").append(value).append("\",");
+        }
+    }
+
+    /**
+     * 排序 Map并拼接字段
+     */
+    private static String traverseMap(Map<Object, Object> map) {
+        StringBuffer sb = new StringBuffer();
+        if (MapUtils.isEmpty(map)) {
+            return sb.append("{}").toString();
+        }
+        Map<Object, Object> sortedMap = sortMapByKey(map);
+        if (MapUtils.isEmpty(sortedMap)) {
+            return sb.append("{}").toString();
+        }
+        sb.append("{");
+        sortedMap.forEach((key, value) -> traverseInsideMap(sb, key, value));
+        return sb.substring(0, sb.length() - 1) + "}";
+    }
+
+    /**
+     * 排序 List并拼接字段
+     */
+    @SuppressWarnings("unchecked")
+    private static String traverseList(List<Object> list) {
+        StringBuffer sb = new StringBuffer();
+        if (CollectionUtils.isEmpty(list)) {
+            return sb.append("[]").toString();
+        }
+        sb.append("[");
+        list.forEach(item -> {
+            if (item instanceof Map) {
+                sb.append(traverseMap((Map<Object, Object>) item));
+            } else if (item instanceof List) {
+                sb.append(traverseList((List<Object>) item));
+            } else {
+                sb.append(item);
+            }
+            sb.append(",");
+        });
+        return sb.substring(0, sb.length() - 1) + "]";
+    }
+
+    private static Map<Object, Object> sortMapByKey(Map<Object, Object> map) {
+        if (map == null || map.isEmpty()) {
+            return null;
+        }
+        // 降序排序
+        Map<Object, Object> sortMap = new TreeMap<>(Comparator.comparing(Object::toString));
+        sortMap.putAll(map);
+        return sortMap;
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        String testString = "{'biz_content': {'mobile': '855010145010', 'couponNo': 'WNJ210908154513449'}, 'app_id': '1624010948354', 'charset': 'UTF-8', 'service': 'send.choice.coupon', 'timestamp': '2020-09-07 16:07:50', 'version': '1.0'}";
+
+        String privateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKfUj13CGKkxEtcmCg3IdQBMyXV46Cdwf+dLX1bmtaAkbPY5ov1zQrNaLICl+tc+zi6L8tqtjA3WS3P9YZiI9FMSCo8lImPrG1RTs7CM1Y6tzl8K/qsecQ44UPvEzlGRfGXttSqdmFYzsSWNIFidz6kXFszCBdYun7EDFnf7WDtXAgMBAAECgYBe/VLLqUDV+h2EwlXsaSm3qr5Xi8AyGl16JtHmWJwx8IvvbL3Qn7z/0CjiA4+O5lBCThl9Jb7gUgrQsnfboqBNues6XyCuiXypubI0sTh0UcrS0dZDSdqlJpGoIlsA2QnYfBEB5Czo+0E4b7+UVBr3F5pYVzu+0iyqcAcQwXUckQJBAPbkjcdYMdAprmis8r24DaX7qvKEDxunlSvW5kpJd+k3+toUpOQtpb7+rNOrlEJFU4SNjYfLgQzUby0AEYyMZJkCQQCuBWg+4lxFzud1tsGmeIu7sp7qwyoutYU2+KBTBvbSs0LD48kOBIDnIztJb8UOi14+ZydSxO32Ac2PiwY98qVvAkBAVjKz/cGNUy9Fy7u9wJad6EUVyV/+ft8ae3eraBW9Sn8uES8e3t5QNSFoT0/lLRekdRaqildotnr6KQhprbQRAkEAnpPs2Akcfry57Wn588I7y3JNIK9yXBgr6dkM+DwLZhvWxn1ndK+j630OhLAmiUd1PTZw/hrYoeoosRrGOGNKXwJBAK6YuCYRZsNd6LNbM/9iTkdJi5LoKwY4mPxegjlAXczdUIx3ylIzF+b36K6Zrr/b/HwEsUYrCpQJr/U1dKBcZs0=";
+        String data = "app_id=1624010948354&biz_content={\"couponNo\":\"WNJ210908154513449\",\"mobile\":\"855010145010\"}&charset=UTF-8&service=send.choice.coupon&timestamp=2020-09-07 16:07:50&version=1.0";
+        System.out.println("UNEnsign data true:" + data);
+        String temp = rsaSignByPrivateKey(testString, privateKey);
+        System.out.println("Ensign data:" + temp);
+    }
+}
