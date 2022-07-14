05f5c6beddc7be1f5ba3a4b9e8cabfa40447831d
windy-gu <408563133@qq.com>
2022-07-14 16:40:13 +0800

    Add HtmlGui and ReportCollector

--- visualizers/src/main/java/org/apache/jmeter/visualizers/ReportCollector.java
+++ visualizers/src/main/java/org/apache/jmeter/visualizers/ReportCollector.java
@@ -1,4 +1,224 @@
 package org.apache.jmeter.visualizers;
 
-public class ReportCollector {
+import org.apache.jmeter.common.cli.CliOptions;
+import org.apache.jmeter.common.utils.TimeUtil;
+import org.apache.jmeter.testelement.AbstractTestElement;
+import org.apache.jmeter.testelement.TestStateListener;
+import org.apache.jmeter.testelement.ThreadListener;
+import org.apache.jmeter.samplers.SampleListener;
+import org.apache.jmeter.samplers.Interruptible;
+import org.apache.jmeter.samplers.SampleEvent;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.services.FileServer;
+import org.apache.jmeter.engine.util.NoThreadClone;
+
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jmeter.visualizers.vo.TestCaseVO;
+import org.apache.jmeter.visualizers.vo.TestStepVO;
+import org.apache.jmeter.visualizers.vo.TestSuiteVO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+/**
+ * @author Windy.Gu
+ */
+public class ReportCollector extends AbstractTestElement
+        implements TestStateListener, ThreadListener, SampleListener, Interruptible, NoThreadClone {
+
+    private static final Logger log = LoggerFactory.getLogger(ReportCollector.class);
+
+    public static final String DATA_FORMAT_PATTERN = "yyyy/MM/dd HH:mm:ss";
+    public static final String REPORT_NAME = "ReportCollector.reportName";
+    public static final String IS_APPEND = "ReportCollector.isAppend";
+
+    public static final String LINKER_SYMBOL = "、";
+
+    private String scriptName;
+    private String reportName;
+
+    public ReportCollector() {
+        super();
+    }
+
+    public ReportCollector(String name) {
+        this();
+        setName(name);
+    }
+
+    @Override
+    public void testStarted() {
+        testStarted("localhost");
+    }
+
+    /**
+     * 测试计划（TestPlan）开始时创建 testSuite
+     */
+    @Override
+    public void testStarted(String host){
+//        scriptName = get
+    }
+
+    @Override
+    public void testEnded() {
+        testEnded("localhost");
+    }
+
+    /**
+     * 测试计划（TestPlan）执行结束后把数据写入 HTML文件中
+     */
+    @Override
+    public void testEnded(String host){
+//        scriptName = get
+    }
+
+    /**
+     * 线程(TestCase)开始时创建 testCase
+     */
+    @Override
+    public void threadStarted(){
+
+    }
+
+    @Override
+    public void threadFinished() {
+        // pass
+    }
+
+    /**
+     * 将测试样本（TestSample）的标题、请求和响应数据写入 HTML
+     */
+    @Override
+    public void sampleOccurred(SampleEvent sampleEvent){
+        SampleResult result = sampleEvent.getResult();
+//        TestSuite
+
+    }
+
+    @Override
+    public void sampleStarted(SampleEvent sampleEvent) {
+        // pass
+    }
+
+    @Override
+    public void sampleStopped(SampleEvent sampleEvent) {
+        // pass
+    }
+
+    private void addSubResultTohtml(SampleResult parentResult, String parentId, TestCaseVo testCase) {
+
+    }
+
+    /**
+     * 获取脚本名称（去文件后缀）
+     */
+    private String getScriptName() {
+        String  scriptName = FileServer.getFileServer().getScriptName();
+        log.debug("scriptName:[ {} ]", scriptName);
+        return scriptName.substring(0, scriptName.length() - 4).trim();
+    }
+
+    @Override
+    public String getThreadName() {
+        return JMeterContextService.getContext().getThread().getThreadName();
+    }
+
+    /**
+     * 获取当前线程的hashCode
+     */
+    private int getThreadHashCode() {
+        return JMeterContextService.getContext().getThread().hashCode();
+    }
+
+    /**
+     * 获取默认报告目录路径 ${JMETER_HOME}/htmlreport
+     */
+    private String getDefaultReportDirectory() {
+        return JMeterUtils.getJMeterHome() + File.separator + "htmlreport";
+    }
+
+    /**
+     * 获取报告目录路径
+     */
+    private String getReportDirectory() {
+        // Non-Gui下，获取命令行的 -JreportDir 选项
+        return JMeterUtils.getPropDefault(CliOptions.REPORT_DIR, getDefaultReportDirectory());
+    }
+
+    private String getReportName() {
+        // Non-Gui下，命令行存在 -JreportName 选项时，优先读取 reportName
+        return JMeterUtils.getPropDefault(CliOptions.REPORT_NAME, getPropertyAsString(REPORT_NAME));
+    }
+
+    private String getIsAppend() {
+        // Non-Gui下，命令行存在 -JisAppend 选项时，优先读取 isAppend
+        return JMeterUtils.getPropDefault(CliOptions.IS_APPEND, getPropertyAsString(IS_APPEND));
+    }
+
+    private boolean isAppend() {
+        return Boolean.parseBoolean(getIsAppend());
+    }
+
+    /**
+     * 获取测试报告路径
+     */
+    private String getReportPath() {
+        return getReportDirectory() + File.separator + addHtmlSuffix(reportName);
+    }
+
+    /**
+     * 为测试报告名称添加.html后缀
+     */
+    private String addHtmlSuffix(String name) {
+        if (name.endsWith(ReportManager.HTML_SUFFIX)) {
+            return name;
+        } else {
+            return name + ReportManager.HTML_SUFFIX;
+        }
+    }
+
+    /**
+     * 控制台打印执行状态信息
+     */
+    private void outputConsole(boolean successful, String message) {
+        if (successful) {
+            System.out.println("[✔][PASSED]- " + message);
+        } else {
+            System.err.println("[✖][FAILED]- " + message);
+        }
+    }
+
+    /**
+     * 获取sampler的耗时
+     *
+     * @param result SampleResult对象
+     * @return 耗时(xx ms)
+     */
+    private String getSampleElapsedTime(SampleResult result) {
+        return result.getEndTime() - result.getStartTime() + "ms";
+    }
+
+    @Override
+    public boolean interrupt() {
+        testEnded();
+        return true;
+    }
+
+    private String getCurrentTime() {
+        return TimeUtil.currentTimeAsString(DATA_FORMAT_PATTERN);
+    }
+
+    /**
+     * 计算耗时
+     *
+     * @param startTime 开始时间
+     * @param endTime   结束时间
+     */
+    private String getElapsedTime(String startTime, String endTime) {
+        return TimeUtil.formatElapsedTimeAsHMS(startTime, endTime, DATA_FORMAT_PATTERN);
+    }
+
 }
--- visualizers/src/main/java/org/apache/jmeter/visualizers/gui/LocalHtmlReportGui.java
+++ visualizers/src/main/java/org/apache/jmeter/visualizers/gui/LocalHtmlReportGui.java
@@ -1,5 +1,205 @@
 package org.apache.jmeter.visualizers.gui;
 
-public class LocalHtmlReportGui {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.gui.util.HorizontalPanel;
+import org.apache.jmeter.common.utils.DesktopUtil;
+import org.apache.jmeter.common.jmeter.JMeterGuiUtil;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jmeter.visualizers.ReportCollector;
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * @author Windy.Gu
+ */
+public class LocalHtmlReportGui extends AbstractListenerGui implements ActionListener {
+    /**
+     * Action命令
+     */
+    private static final String OPEN_HTML_ACTION = "OPEN_HTML";
+    private static final String OPEN_DIRECTORY_ACTION = "OPEN_DIRECTORY";
+
+    /**
+     * swing组件
+     */
+
+    private final JTextField reportNameTextField;
+    private final JLabel reportNameLabel;
+
+
+    private final JComboBox<String> isAppendComboBox;
+    private final JLabel isAppendLabel;
+
+    /**
+     * HTML目录路径
+     */
+    private final String reportDirectory;
+
+    /**
+     * 插件说明
+     */
+    private static final String NOTE =
+            "1、测试报告生成路径为${JMETER_HOME}/htmlreport/${reportName}\n" +
+                    "2、执行前必须先在 ${JMETER_HOME} 下创建 htmlreport 目录\n" +
+                    "3、Non-Gui命令说明：\n" +
+                    "    3.1、存在 -JreportName 选项时，优先读取 ${__P(reportName)} HTML报告名称\n" +
+                    "    3.2、存在 -JisAppend 选项时，优先读取 ${__P(isAppend)} 追加模式\n" +
+                    "4、如中文乱码，在 ${JMETER_BIN}/jmeter[.bat|sh]里添加Java启动参数 \"-Dfile.encoding=UTF-8\"\n";
+
+    public LocalHtmlReportGui() {
+        reportDirectory = JMeterUtils.getJMeterHome() + File.separator + "htmlreport";
+
+        reportNameTextField = createReportNameTextField();
+        reportNameLabel = createReportNameLabel();
+
+        isAppendComboBox = createIsAppendComboBox();
+        isAppendLabel = createIsAppendLabel();
+
+        init();
+    }
+
+    private void init() {
+        setLayout(new BorderLayout());
+        setBorder(makeBorder());
+        add(makeTitlePanel(), BorderLayout.NORTH);
+        add(createBodyPanel(), BorderLayout.CENTER);
+        add(createNoteArea(), BorderLayout.SOUTH);
+    }
+
+    @Override
+    public String getStaticLabel() {
+        return "HTML 报告";
+    }
+
+    @Override
+    public String getLabelResource() { return null;}
+
+    @Override
+    public TestElement createTestElement() {
+        ReportCollector info = new ReportCollector();
+        modifyTestElement(info);
+        return info;
+    }
+
+    /**
+     * TestElement -> GUI  界面与Sampler之间的数据交换
+     */
+    @Override
+    public void configure(TestElement el) {
+        super.configure(el);
+        reportNameTextField.setText(el.getPropertyAsString(ReportCollector.REPORT_NAME));
+        isAppendComboBox.setSelectedItem(el.getPropertyAsString(ReportCollector.IS_APPEND));
+    }
+
+    /**
+     * GUI -> TestElement  用于把界面的数据移到Sampler中
+     */
+    @Override
+    public void modifyTestElement(TestElement el) {
+        super.configureTestElement(el);
+        el.setProperty(ReportCollector.REPORT_NAME, reportNameTextField.getText());
+        el.setProperty(ReportCollector.IS_APPEND, (String) isAppendComboBox.getSelectedItem());
+    }
+
+    /**
+     * 该方法会在reset新界面的时候调用，这里可以填入界面控件中需要显示的一些缺省的值(就是默认显示值)
+     */
+    @Override
+    public void clearGui() {
+        super.clearGui();
+        reportNameTextField.setText("");
+        isAppendComboBox.setSelectedItem("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals(OPEN_HTML_ACTION)) {
+            openHtml();
+        } else if (action.equals(OPEN_DIRECTORY_ACTION)) {
+            openHtmlDirectory();
+        }
+    }
+
+    private void openHtml() {
+        String reportName = reportNameTextField.getText();
+        if (StringUtils.isBlank(reportName)) {
+            return;
+        }
+        DesktopUtil.openFile(reportDirectory + File.separator + reportName);
+    }
+
+    private void openHtmlDirectory() {
+        DesktopUtil.openFile(reportDirectory);
+    }
+
+    private JTextField createReportNameTextField() {
+        return JMeterGuiUtil.createTextField(ReportCollector.REPORT_NAME);
+    }
+
+    private JLabel createReportNameLabel() {
+        return JMeterGuiUtil.createLabel("报告名称：", reportNameTextField);
+    }
+
+    private JComboBox<String> createIsAppendComboBox() {
+        JComboBox<String> comboBox = JMeterGuiUtil.createComboBox(ReportCollector.IS_APPEND);
+        comboBox.addItem("False");
+        comboBox.addItem("True");
+        return comboBox;
+    }
+
+    private JLabel createIsAppendLabel() {
+        return JMeterGuiUtil.createLabel("追加写报告：", isAppendComboBox);
+    }
+
+    private Component createBodyPanel() {
+        JPanel bodyPanel = new JPanel(new GridBagLayout());
+        bodyPanel.setBorder(JMeterGuiUtil.createTitledBorder("配置HTML"));
+
+        bodyPanel.add(reportNameLabel, JMeterGuiUtil.GridBag.labelConstraints);
+        bodyPanel.add(reportNameTextField, JMeterGuiUtil.GridBag.editorConstraints);
+
+        bodyPanel.add(isAppendLabel, JMeterGuiUtil.GridBag.labelConstraints);
+        bodyPanel.add(isAppendComboBox, JMeterGuiUtil.GridBag.editorConstraints);
+
+        VerticalPanel mainPanel = new VerticalPanel();
+        mainPanel.add(bodyPanel);
+        mainPanel.add(createButtonPanel());
+        return mainPanel;
+
+    }
+
+    private JPanel createButtonPanel() {
+        HorizontalPanel buttonPanel = new HorizontalPanel();
+        buttonPanel.setBorder(JMeterGuiUtil.createTitledBorder("操作"));
+        buttonPanel.add(createOpenHtmlButton());
+        buttonPanel.add(createOpenDirectoryButton());
+        return buttonPanel;
+    }
+
+    private Component createOpenHtmlButton() {
+        JButton button = new JButton("打开报告");
+        button.setActionCommand(OPEN_HTML_ACTION);
+        button.addActionListener(this);
+        return button;
+    }
+
+    private Component createOpenDirectoryButton() {
+        JButton button = new JButton("打开HTML目录");
+        button.setActionCommand(OPEN_DIRECTORY_ACTION);
+        button.addActionListener(this);
+        return button;
+    }
+
+    private Component createNoteArea() {
+        return JMeterGuiUtil.createNoteArea(NOTE, this.getBackground());
+    }
 
 }
